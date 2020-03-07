6b516c0132e06ef7398f28251c93bb4de66c0e77
Marsha Annon <marshaannon@gmail.com>
2020-03-07 13:33:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,10 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Your code here
+  planeteer_calls.map do |call|
+    puts "#{call}.upcase!, "
+
+  end
 end
 
 def long_planeteer_calls# code an argument here
