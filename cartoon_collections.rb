f4e11631d2ccd4d19a1cfd9961d6fb4ebc90c36a
Marsha Annon <marshaannon@gmail.com>
2020-03-07 14:24:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(snacks)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find{|snack| snack = cheese-types}
 end
