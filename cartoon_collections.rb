9f8f7897e5d9abd2621ea93018dc9fb7456e05aa
Marsha Annon <marshaannon@gmail.com>
2020-03-07 14:39:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,5 @@ end
 
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  snacks.include?("cheese_types")
-    return snacks
+  snacks.find {|cheese| cheese_types.include?(cheese)}
 end
